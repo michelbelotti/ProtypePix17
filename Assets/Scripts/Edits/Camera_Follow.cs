069e2f16abcefab2525afb87717f7d53dd22a94e
michelbelotti <michelbelotti@gmail.com>
2019-01-10 14:09:40 -0200

    Correcoes de scripts e modificacao de alguns prefabs

---
+++ Assets/Scripts/Edits/Camera_Follow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    public float maxHeight = 100;
+    public float minHeight = 100;
+
+    public float maxWidth = 100;
+    public float minWidth = 100;
+
+    private Vector3 initialPos;
+
+    private void Start()
+    {
+        initialPos = target.position;
+    }
+
+    void FixedUpdate () {
+
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+
+        transform.position = new Vector3(Mathf.Clamp(smoothedPos.x, (initialPos.x - minWidth), (initialPos.x + maxWidth)), Mathf.Clamp(smoothedPos.y, (initialPos.y - minHeight), (initialPos.y + maxHeight)), smoothedPos.z);
+        //transform.LookAt(target);
+    }
+}
--- Assets/Scripts/Edits/Player_Movement.cs
+++ Assets/Scripts/Edits/Player_Movement.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_movement : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
     //[SerializeField]
     //float angle;
-
+    public bool invert_Yaxis = true;
     public float forward_speed = 20;
     public float mov_speed = 25;
     public float maneuver_speed = 5.0f;
@@ -24,9 +24,11 @@ public class player_movement : MonoBehaviour
     private float input_Vertical;
     private float input_Horizontal;
 
+    private Vector3 initialPos;
+
     private void Start()
     {
-
+        initialPos = transform.position;
     }
 
     private void Update()
@@ -45,36 +47,32 @@ public class player_movement : MonoBehaviour
 
         maneuverControls();
 
-        transform.position += transform.forward * forward_speed * Time.deltaTime;
+        //transform.position += transform.forward * forward_speed * Time.deltaTime;
+        transform.position += Vector3.forward * forward_speed * Time.deltaTime;
     }
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, clampWidthMin, clampWidthMax), Mathf.Clamp(transform.position.y, clampHeightMin, clampHeightMax), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, initialPos.x - clampWidthMin, initialPos.x + clampWidthMax), Mathf.Clamp(transform.position.y, initialPos.y - clampHeightMin, initialPos.y + clampHeightMax), transform.position.z);
     }
 
     private void MoveVertAxis()
     {
         Vector3 direction = new Vector3(0, input_Vertical, 0);
-        
-        transform.position += direction * mov_speed * Time.fixedDeltaTime;
+
+        transform.position += (invert_Yaxis ? -1 : 1) * direction * mov_speed * Time.fixedDeltaTime;
     }
 
     private void MoveHorAxis()
     {
         Vector3 direction = new Vector3(input_Horizontal, 0, 0);
-        //Vector3 direction = new Vector3(input_Horizontal, 0, 0);
         
         transform.position += direction * mov_speed * Time.fixedDeltaTime;
-        //transform.position += direction * mov_speed * Time.fixedDeltaTime;
-
     }
-
-
 
     private void maneuverControls()
     {
-        Quaternion target = Quaternion.Euler((input_Vertical * -1 * max_angle_pitch), (input_Horizontal * max_angle_roll), (input_Horizontal * -1 * max_angle_yaw));
+        Quaternion target = Quaternion.Euler((input_Vertical * (invert_Yaxis ? 1 : -1) * max_angle_pitch), (input_Horizontal * max_angle_yaw), (input_Horizontal * -1 * max_angle_roll));
 
         transform.rotation = Quaternion.Slerp(transform.rotation, target, maneuver_speed * Time.deltaTime);
     }
